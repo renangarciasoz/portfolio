d8c40eb693936f004b5e2d852677caa19edadd16
renangarciasoz <renangarciasoz@hotmail.com>
2022-03-14 16:07:12 -0300

    resolve serveSideTranslations on vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,12 @@
 // next-i18next library configuration
 // https://github.com/isaachinman/next-i18next
+const path = require("path");
 
 module.exports = {
   i18n: {
     reloadOnPrerender: true, // reload texts when reload page.
     locales: ["en", "pt-BR"],
     defaultLocale: "en",
+    localePath: path.resolve("./public/static/locales"),
   },
 };
